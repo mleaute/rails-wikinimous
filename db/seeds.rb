9bef10c99d6d8d3cef61a7333c741bafd8aa3599
Marine Leaute <leautemarine@yahoo.fr>
2018-08-07 16:36:47 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#  Character.create(name: 'Luke', movie: movies.first)
+
+Article.create title:"Jerem Star", content:"Une star en or..."
